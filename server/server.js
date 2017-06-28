213339dfcf6a3bbddc6d767b8f90ba3174259f07
Jennifer Le Pham <jennifer.pham@glidewelldental.com>
2017-06-28 01:02:06 -0700

    io event listeners

--- server/server.js
+++ server/server.js
@@ -1,18 +1,25 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, "../public/" ); //resolves file structure without dots
-
 require('../config/config');
 const port = process.env.PORT || 3000; //process.env.PORT comes from config/config.json.  Need this for heroku
 
-
-
-
 var app = express();
+var server = http.createServer(app); //app.listen() also calls http.createServer(app) (express uses this)
+var io = socketIO(server); //access to localhost:3000 port and also to socket.io/socket.iojs
 
 app.use(express.static(publicPath)); //configure express static middleware that serves up public folder
 
+io.on('connection', (socket) => { //register an event listener and do something when that event listens. listen for new connection.  socket represents individual socket. if one is down, they keep trying to reconnect
+    console.log('new user connected');
+
+    socket.on('disconnect', ()=> {
+        console.log('User was disconnected - gfrom client');
+    })
+})
 // app.get('/', function (req, res){
 //     res.sendFile(path.join(publicPath + 'index.html'));
 // })
@@ -22,6 +29,6 @@ app.use(express.static(publicPath)); //configure express static middleware that 
 
 //app.listen on port and console.log port #
 //app.listen(3000); (doesn't need callback function)
-app.listen(port, () => {
+server.listen(port, () => {
     console.log('starting app on port ', port);
 })
